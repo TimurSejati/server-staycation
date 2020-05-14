1b0e770b41b49a1644edfd7a97b1a93411b20232
TimurSejati <timur7dian84@gmail.com>
2020-05-14 13:00:15 +0700

    modified app.js

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors());
 app.use(methodOverride("_method"));
 app.use(
   session({
@@ -56,7 +57,6 @@ app.use("/users", usersRouter);
 // admin
 app.use("/admin", adminRouter);
 app.use("/api/v1/member", apiRouter);
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
